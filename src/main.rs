6d4be05cb0505e151fcd8d26acc5956a3fb054a0
Perfspec <lewisrwebb@gmail.com>
2020-06-18 00:35:42 +0200

    v0.2.0 use struct MultiTeamGame with impl functions to organize behaviour

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,40 @@
+use std::collections::HashMap;
+
 fn main() {
-    use std::collections::HashMap;
 	
-	let teams = vec![String::from("Green"), String::from("Red")];
+	let intial_teams = vec![String::from("Green"), String::from("Red")];
     let initial_scores = vec![11, 22];
 	
-    let mut scores: HashMap<_, _> =
-        teams.into_iter().zip(initial_scores.into_iter()).collect();
-
-    scores.insert(String::from("Blue"), 33);
-    scores.insert(String::from("Yellow"), 44);
+    let mut game = MultiTeamGame {
+        scores: intial_teams.into_iter().zip(initial_scores.into_iter()).collect()
+	};
 	
-	println!("{:#?}", scores);
+	game.create_score(String::from("Blue"), 33);
+    game.create_score(String::from("Yellow"), 44);
+	
+	game.get_all_scores();
+	
+	let team_name_searches = vec![String::from("Blue"), String::from("Orange")];
+	
+	game.get_score(&team_name_searches[0]);
+	game.get_score(&team_name_searches[1]);
+}
+
+struct MultiTeamGame {
+	scores: HashMap<String, usize>
+}
+
+impl MultiTeamGame {
+	fn create_score(&mut self, team_name: String, team_score: usize) {
+		self.scores.insert(team_name,team_score);
+	}
+	fn get_all_scores(&self) {
+		println!("scores: {:#?}", self.scores);
+	}
+	fn get_score(&self, team_name: &String) {
+		match self.scores.get(team_name) {
+			Some(value) => println!("The score for {} is {}", team_name, value),
+			None => println!("No score found for {}", team_name),
+		}
+	}
 }
